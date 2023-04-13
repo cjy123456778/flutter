687af38799bd8fd272616d8810a4b7f6438abc7f
chenjiangyuan <1020752627@qq.com>
2023-04-13 15:37:56 +0800

    精简flutter项目 [提交人]cjy [版本]1.2

---
+++ flutter_test/app/src/main/java/com/example/flutter_test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.flutter_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+    }
+}
